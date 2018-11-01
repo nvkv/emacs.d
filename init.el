61b3b07eb24e35da87c05e661ce4015f26d452e9
Semyon Novikov <me@sdfgh153.ru>
2018-11-01 13:52:58 +0500

    Hello, org

--- init.el
+++ init.el
@@ -7,6 +7,36 @@
       `(,@package-archives
         ("melpa" . "https://melpa.org/packages/")))
 
+;; Org settings
+(setq org-directory "~/Dropbox/org/")
+(setq org-notes-file (concat org-directory "notes.org"))
+(setq org-todo-file (concat org-directory "todo.org"))
+(setq org-agenda-files (file-expand-wildcards (concat org-directory "*.org")))
+(setq org-default-notes-file (concat org-directory "inbox.org"))
+(setq org-refile-targets '((nil :maxlevel . 9)
+                                (org-agenda-files :maxlevel . 9)))
+
+(setq org-capture-templates
+ '(("t" "Todo" entry (file+headline org-default-notes-file "Inbox")
+        "* TODO %?\n  %U")
+   ("j" "Journal" entry (file+olp+datetree "~/org/journal.org")
+    "* %?\nEntered on %U\n  %i\n  %a")))
+
+(define-key global-map "\C-cc" 'org-capture)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-ci" '(lambda ()
+													 (interactive)
+													 (find-file org-default-notes-file)))
+
+(global-set-key "\C-ct" '(lambda ()
+													 (interactive)
+													 (find-file org-todo-file)))
+
+(global-set-key "\C-cn" '(lambda ()
+													 (interactive)
+													 (find-file org-notes-file)))
+
+
 (package-initialize)
 
 (setq package-enable-at-startup nil)
@@ -22,6 +52,11 @@
 (setq-default tab-width 2)
 (setq indent-tabs-mode nil)
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+(global-set-key (kbd "C-c r") (lambda ()
+                                (interactive)
+                                (revert-buffer t t t)
+                                (message "buffer is reverted")))
 
 (setq backup-directory-alist
       `((".*" . ,temporary-file-directory)))
@@ -144,6 +179,9 @@
  '(custom-safe-themes
 	 (quote
 		("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
+ '(org-agenda-files
+	 (quote
+		("~/Dropbox/org/inbox.org" "~/Dropbox/org/maybe.org" "~/Dropbox/org/notes.org" "~/Dropbox/org/todo.org")))
  '(package-selected-packages
 	 (quote
 		(magit ag zzz-to-char zap-to-char zop-to-char json-mode gradle-mode ob-clojurescript which-key cider soft-stone-theme stekene minimal-theme monochrome monochrome-theme farmhouse-theme basic-theme eziam-common eziam-theme github-modern-theme dockerfile-mode markdown-mode go-mode eink-theme github-theme yaml-mode use-package terraform-mode solarized-theme projectile git-commit editorconfig auto-complete))))
